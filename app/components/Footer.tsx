5d7287ed44cab0f737b45a3e95c4ab77b11f4a20
juinlaw <cantury77@gmail.com>
2026-04-29 14:07:57 +0900

    update footer brand name

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,7 @@ export default function Footer() {
   return (
     <footer className="daeon-footer">
       <div className="daeon-footer-inner">
-
         {/* 로고 */}
-
         <div className="daeon-footer-logo">
           <img
             src="/images/logo.png"
@@ -13,18 +11,14 @@ export default function Footer() {
           />
         </div>
 
-
         {/* 기본 정보 */}
-
         <div className="daeon-footer-info">
           대표변호사 : 신동우 | 주소 : 서울 서초구 서초대로 250
           스타갤러리브릿지빌딩 802호 | 전화번호 : 02-6952-3695 |
           이메일 : noleosi@daeonlaw.co.kr
         </div>
 
-
         {/* 네이버 카페 링크 */}
-
         <div className="daeon-footer-cafe">
           최신 금융사기 피해 사례와 대응 정보는&nbsp;
           <a
@@ -37,13 +31,10 @@ export default function Footer() {
           </a>
         </div>
 
-
         {/* 저작권 */}
-
         <div className="daeon-footer-copy">
           COPYRIGHT © 2024 대온 법률사무소 All rights reserved.
         </div>
-
       </div>
     </footer>
   )
